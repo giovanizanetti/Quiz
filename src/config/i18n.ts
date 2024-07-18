99835cfc4592280edd311095c81f480e4e86ad0b
Giovani Zanetti <g.zanetti@picturae.com>
2024-07-18 08:50:49 +0200

    setup locales

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,11 +1,13 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import HttpBackend from 'i18next-http-backend'
+import { LANGUAGE } from '../constants'
 
 i18n
   .use(HttpBackend)
+  .use(LanguageDetector)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: LANGUAGE.en,
     interpolation: {
       escapeValue: false,
     },
