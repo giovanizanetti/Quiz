9005904f44b4090043b6625bf9206ce62ca41474
Giovani Zanetti <g.zanetti@picturae.com>
2024-07-20 13:05:31 +0100

    show results page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './views/Home'
 import { NotFound } from './components/NotFound'
 import { NavBar } from './components/NavBar'
 import { Quiz } from './views/Quiz'
+import { Results } from './views/Results'
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Routes>
           <Route  path="/" element={<Home />} />
           <Route path="quiz/question/:questionNumber" element={<Quiz />} />
+          <Route path="quiz/results" element={<Results />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
